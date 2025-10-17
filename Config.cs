1a3d91313a08470afb7f01bfe25706a35ffdd799
PiwnicaUwU <piwnicagaming21372@gmail.com>
2025-10-17 17:57:46 +0200

    Nowe pole "Scp035HintDuration"

--- Config.cs
+++ Config.cs
@@ -10,6 +10,7 @@ namespace Scp035
         public string Scp035ItemPickedUpHint { get; set; } = "<b><color=red>You picked up SCP-035.</color></b>";
         public string Scp035ItemChangedHint { get; set; } = "<b><color=red>You selected SCP-035.</color></b>";
         public string Scp035Hint { get; set; } = "<b><color=red>You are SCP-035</color></b>";
+        public float Scp035HintDuration { get; set; } = 15f;
         public RoomName[] Scp035ItemSpawnRooms { get; set; } = { RoomName.Hcz049, RoomName.Hcz079, RoomName.Hcz096 };
     }
 }
